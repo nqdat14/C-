142b809df7e9ca01959c74815eb793e305da1398
nqdat <nguyenquocdat123654@gmail.com>
2024-08-23 20:25:39 +0700

    program

--- buoi2/buoi2/Program.cs
+++ buoi2/buoi2/Program.cs
@@ -10,6 +10,8 @@ namespace buoi2
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
